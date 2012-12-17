fbc0d360c4cdddae963bbffd2153bc0c0013cdd6
Joshua Hunter <mastershihochief@vps12.ecst.csuchico.edu>
2012-12-17 08:50:24 -0800

    Administar

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,6 +1,7 @@
 class UsersController < ApplicationController
-before_filter :authenticator, :only => [:edit, :update]
+before_filter :authenticator, :only => [:edit, :update, :index, :destroy]
 before_filter :correct_user,  :only => [:edit, :update]
+before_filter :admin_user,    :only => :destroy
 def new
     @title = "Sign up"
 	@user = User.new
@@ -37,6 +38,15 @@ def create
   end
 end
  
+def index
+@users = User.paginate(:page =>1)
+@title = "User List"
+end
+
+ def destroy
+    @user.destroy
+    redirect_to users_path, :flash => { :success => "TARGET DESTROYED!" }
+end
 private
 
 def authenticator
@@ -47,4 +57,9 @@ def correct_user
 	@user = User.find(params[:id])
 	redirect_to root_path, :notice => "UNAUTHORIZED ACCESS DETECTED, Editing other user accounts is forbidden" unless current_user?(@user)
 end
+
+ def admin_user
+      @user = User.find(params[:id])
+      redirect_to(root_path) if !current_user.admin? || current_user?(@user)
+    end
 end
